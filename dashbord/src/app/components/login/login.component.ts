8a65ed2e9ffdb10ca0c686d6d4bd6346d7d155bb
AlaAydi <contact.aydiala@gmail.com>
2025-11-23 21:43:43 +0100

    login component done

---
+++ dashbord/src/app/components/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true, 
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+      console.log('Formulaire invalide');
+    }
+  }
+
+}
